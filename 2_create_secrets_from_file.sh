5dce23c8ad295de8e45173016483e8deb95a822d
Guy Barros <38210175+GuyBarros@users.noreply.github.com>
2021-01-20 14:14:02 +0000

    Update 2_create_secrets_from_file.sh

--- 2_create_secrets_from_file.sh
+++ 2_create_secrets_from_file.sh
@@ -8,7 +8,7 @@ while read HOSTNAME IPADDRESS PROJ_USERNAME PROJ_PASSWORD BREAKGLASS_USERNAME BR
 do
     if [ ! $HOSTNAME == "HOSTNAME" ]
     then
-    curl -X PUT -H "X-Vault-Request: true" -H "X-Vault-Namespace: DataCenter1" -H "X-Vault-Token: $(echo $VAULT_TOKEN)" -d '{"data":{ "ipaddress":"'$(echo $IPADDRESS)'","breakglass_username":"'$(echo $BREAKGLASS_USERNAME)'","breakglass_password": "'$(echo $BREAKGLASS_PASSWORD)'","proj_password":"'$(echo $PROJ_PASSWORD)'","proj_username":"'$(echo $PROJ_USERNAME)'","notes":"'$(echo $NOTES)'"},"options":{}}' $(echo $VAULT_ADDR)/v1/servers/data/$(echo $HOSTNAME)
+    curl -X PUT -H "X-Vault-Request: true" -H "X-Vault-Namespace: $(echo $VAULT_DATACENTER)" -H "X-Vault-Token: $(echo $VAULT_TOKEN)" -d '{"data":{ "ipaddress":"'$(echo $IPADDRESS)'","root_password":"'$(echo $ROOT_PASSWORD)'","root_username":"'$(echo $ROOT_USERNAME)'"},"options":{}}' $(echo $VAULT_ADDR)/v1/$(echo VAULT SECRET PATH)/data/$(echo $HOSTNAME)
     echo "saved $HOSTNAME" in vault
     fi
 done < $INPUT
